e8e8088cab54085a58948f1d88baac1c5c57c349
sephy <jerxelcabillon>
2025-08-02 19:00:57 +0800

    add images and update source files

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,29 @@ public class Main {
             sum += nums[i];
         }
         System.out.println(sum);
+
+        int [][]arr = {
+                {1,2,3,},
+                {4,5,6,},
+                {7,8,9},
+                {10,11,12}
+        };
+
+        System.out.println();
+
+        for(int i = 0 ; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        for (int[] row : arr){
+            for(int elem : row){
+                System.out.print(elem + " ");
+            }   s
+            System.out.println();
+        }
     }
 }
---
+++ src/Readme.md
@@ -0,0 +1,6 @@
+# Jerxel Cabillon
+- C2B
+- GT: Arrays
+## Output 
+![img.png](../image/img.png)
+![img_1.png](../image/img_1.png)
